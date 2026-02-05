8472ac5ff3b98be161b622229418bb18428288ee
Rafael Afonso <122814044+rafae1-afonso@users.noreply.github.com>
2026-02-05 19:58:13 -0300

    Estrutura mobile first e responsividade
    Foi atualizada a pagina inicial para ter uma estrutura mobile first e responsividade

--- src/components/organisms/Header/index.tsx
+++ src/components/organisms/Header/index.tsx
@@ -4,7 +4,7 @@ import { FaBars } from 'react-icons/fa6'
 import { PiGearLight, PiPackageLight, PiShoppingCartLight, PiUserCircleFill } from 'react-icons/pi'
 
 const Header = () => {
-    return <header className="w-screen h-18 absolute top-0 flex items-center justify-between bg-light text-black px-10 shadow-xl">
+    return <header className="w-screen h-18 absolute top-0 flex items-center bg-light text-black px-10 shadow-xl">
 
         <div className="flex items-center gap-10">
 
@@ -17,26 +17,26 @@ const Header = () => {
             }>
                 <FaBars size={35} className="" />
             </SideModal>
-
-            <div className={`w-full text-center text-2xl font-semibold text-dark`}>
-                ALL
-                <span className="font-bold text-4xl allbuyGradientText">BUY</span>
-                <span className="text-sm">&copy;</span>
-            </div>
         </div>
 
-        <div className="flex justify-start items-center gap-10">
+        <div className={`ml-10 text-center text-2xl font-semibold text-dark`}>
+            ALL
+            <span className="font-bold text-4xl allbuyGradientText">BUY</span>
+            <span className="text-sm">&copy;</span>
+        </div>
+
+        <div className="mx-55 flex justify-start items-center gap-10 mobileDisplay md:block">
             <SearchComponent />
         </div>
 
-        <div className="flex justify-start items-center gap-5">
-            <button className="duration-100 cursor-pointer hover:text-gray-400 active:text-allbuy-blue-1">
+        <div className="ml-auto flex justify-start items-center gap-5">
+            <button className="duration-100 cursor-pointer hover:text-gray-400 active:text-allbuy-blue-1 mobileDisplay md:block">
                 <PiPackageLight size={35} />
             </button>
-            <button className="duration-100 cursor-pointer hover:text-gray-400 active:text-allbuy-blue-1">
+            <button className="duration-100 cursor-pointer hover:text-gray-400 active:text-allbuy-blue-1 mobileDisplay md:block">
                 <PiShoppingCartLight size={35} />
             </button>
-            <button className="duration-100 cursor-pointer hover:text-gray-400 active:text-allbuy-blue-1">
+            <button className="duration-100 cursor-pointer hover:text-gray-400 active:text-allbuy-blue-1 mobileDisplay md:block">
                 <PiGearLight size={35} />
             </button>
             <button className="duration-100 cursor-pointer hover:text-gray-400 active:text-allbuy-blue-1">
